8514f8be7374e29041ac51f79440583a2d54a9c6
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-05-09 19:42:25 +0100

    Guard routes

--- Roll of the Dice/Assets/Scripts/GuardAI.cs
+++ Roll of the Dice/Assets/Scripts/GuardAI.cs
@@ -9,9 +9,11 @@ public class GuardAI : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
+    //public Vector3 walkPoint;
+    public Vector3[] waypoints;
+    public int currentWaypoint = 0;
+    //bool walkPointSet;
+    //public float walkPointRange;
     public float sightRange;
     public bool playerInSightRange;
 
@@ -32,7 +34,7 @@ public class GuardAI : MonoBehaviour
 
     private void Patrol()
     {
-        if(!walkPointSet)
+        /*if(!walkPointSet)
         {
             SearchWalkPoint();
         } else
@@ -43,19 +45,29 @@ public class GuardAI : MonoBehaviour
         if(distanceToWalkPoint.magnitude < 1f)
         {
             walkPointSet = false;
+        }*/
+        agent.SetDestination(waypoints[currentWaypoint]);
+        Vector3 distanceToWaypoint = transform.position - waypoints[currentWaypoint];
+        if(distanceToWaypoint.magnitude < 1f)
+        {
+            currentWaypoint++;
+            if(currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
         }
     }
 
     private void SearchWalkPoint()
     {
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        /*float randomX = Random.Range(-walkPointRange, walkPointRange);
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
         if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
         {
             walkPointSet = true;
-        }
+        }*/
     }
 
 }
