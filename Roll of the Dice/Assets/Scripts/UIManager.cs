13e392653920dbd51097d9f6549f91af44c4b2f2
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-05-10 22:25:23 +0100

    Audio + Menu stuff
    Audio volume and position in track now persists through scenes. Settings dialog and menu buttons are animated + play sounds

--- Roll of the Dice/Assets/Scripts/UIManager.cs
+++ Roll of the Dice/Assets/Scripts/UIManager.cs
@@ -5,8 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+
+    public Animator buttons;
+    public Animator dialog;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
     }
+
+    public void OpenSettings()
+    {
+        buttons.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        buttons.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
 }
